339143f2ac2977d7e22c598f6cfa0255400ee44b
Ghasem Shirdel <ghasem79.dev@gmail.com>
2022-12-25 17:51:33 +0330

    Update changelog-generator.sh

--- .github/changelog-generator.sh
+++ .github/changelog-generator.sh
@@ -3,44 +3,38 @@
 # >> chmod +x ./changelog-generator.sh
 
 # Check the current folder is a git repository
-$(git -C $PWD rev-parse)
-if [[ $? != 0 ]]; then
-    exit 1
+if ! git -C "$PWD" rev-parse; then
+  exit 1
 fi
 
 # Color formatting
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-RESET="\033[0m"
+RED="\e[31m"
+GREEN="\e[32m"
+RESET="\e[0m"
 
 # Handel input parameters
-if [ $? == 0 ]; then
-    case "$1" in
-    -h | help)
-        echo "help :)"
-        ;;
+case "$1" in
+-h | help)
+  printf "${GREEN}%s${RESET}\n" "help :)"
+  exit 0
+  ;;
 
-    -cf=*)
-        CONFIG_FILE_PATH=$(cut -c 5- <<<"$1")
-        ;;
+-cf=*)
+  CONFIG_FILE_PATH=$(cut -c 5- <<<"$1")
+  ;;
 
-    config-file=*)
-        CONFIG_FILE_PATH=$(cut -c 13- <<<"$1")
-        ;;
+config-file=*)
+  CONFIG_FILE_PATH=$(cut -c 13- <<<"$1")
+  ;;
 
-    *)
-        echo $"Usage: $0 {help(-h)|config-file(-cf)}"
-        exit 1
-        ;;
+*)
+  printf "${RED}Usage: %s {help(-h)|config-file(-cf)}${RESET}\n" "$0"
+  exit 1
+  ;;
 
-    esac
-else
-    printf "\n${RED}An error occurred. Please try again.${RESET}\n"
-    exit 1
+esac
+
+if pip list | grep -F pyyaml; then
+  pip install pyyaml
 fi
-
-# TODO install pyyaml
-# if [pip  list | grep -F pyyaml]; then
-#     pip install pyyaml
-# fi
-python3 ~/changelog-generator/.github/commit-parser.py "$CONFIG_FILE_PATH"
+python3 configs/changelog/commit-parser.py "$CONFIG_FILE_PATH"
